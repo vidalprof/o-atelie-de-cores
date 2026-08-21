3d6da25ae3849984fe28197667bb2152e1b024ba
Entregador <entregador@users.noreply.github.com>
2026-08-21 18:12:38 +0000

    Atualiza a atividade

--- sw.js
+++ sw.js
@@ -3,7 +3,7 @@
    ao index.html do cache em vez da "pagina sem conexao"); CACHE PRIMEIRO
    em imagem/audio. O HASH no nome do cache troca a cada build. */
 var PREFIXO="co-";
-var CACHE=PREFIXO+"3d976b04e8";
+var CACHE=PREFIXO+"dddc4de822";
 var ATIVOS=["./","./index.html"];
 self.addEventListener("install",function(e){self.skipWaiting();e.waitUntil(caches.open(CACHE).then(function(c){return c.addAll(ATIVOS).catch(function(){});}));});
 self.addEventListener("activate",function(e){e.waitUntil(caches.keys().then(function(ks){return Promise.all(ks.map(function(k){if(k!==CACHE&&k.indexOf(PREFIXO)===0)return caches.delete(k);}));}));self.clients.claim();});
